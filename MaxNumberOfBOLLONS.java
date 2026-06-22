a2073226e2e598346a490ff60cfe7981fafac2a0
Rokesh Venkat <rokeevenkat@gmail.com>
2026-06-22 23:26:55 +0530

    Implement method to count max 'balloon' words

---
+++ MaxNumberOfBOLLONS.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+        for(char ch : text.toCharArray()){
+            switch(ch){
+                case 'b' : b++;
+                           break;
+                case 'a' : a++;
+                           break;
+                case 'l' : l++;
+                           break;
+                case 'o' :  o++;
+                           break;
+                case 'n' : n++;
+                           break;
+            }
+        }
+        l=l/2;
+        o=o/2;
+        return Math.min(b,Math.min(a,Math.min(l,Math.min(o,n))));
+         
+    }
+}
