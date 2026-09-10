6e7cd1269f38eb2159fad912f7f5eae30367a129
Rokesh Venkat <rokeevenkat@gmail.com>
2026-09-11 01:19:56 +0530

    Time: 1 ms (98.01%) | Memory: 43.8 MB (15.57%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int evensum=0;
+        int oddsum=0;
+        for(int i=0;i<num.length();i++){
+            int n=num.charAt(i) - '0' ;
+            if(i%2==0){
+                evensum+=n;
+            }else{
+                oddsum+=n;
+            }
+        }
+
+        if(evensum==oddsum){
+            return true;
+        }
+        return false;
+    }
+
+}
